4fa31bbbd97555d37b8fa70c817edcdef786dea8
RODRIGO MOTOLLO SIQUEIRA <rodrigo.motollo@bgp.local>
2024-07-23 14:14:56 -0300

    Adicionando novas cadeias

---
+++ langchain_lcel.py
@@ -0,0 +1,56 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import SimpleSequentialChain, LLMChain
+from langchain.globals import set_debug
+import os
+from dotenv import load_dotenv
+from langchain_core.pydantic_v1 import Field, BaseModel
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+
+load_dotenv()
+set_debug(True)
+
+class Destino(BaseModel):
+    cidade = Field("Cidade a visitar")
+    motivo = Field("Motivo pelo qual é interessante visitar a cidade")
+
+
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0.7,
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+parseador = JsonOutputParser(pydantic_object=Destino)
+
+modelo_cidade = PromptTemplate(
+    template= """Sugira uma cidade dado meu interesse por {interesse}
+    {formatacao_de_saida}
+    
+    """,
+    input_variables=["interesse"],
+    partial_variables={"formatacao_de_saida": parseador.get_format_instructions()}
+)
+
+modelo_restaurantes = ChatPromptTemplate.from_template(
+    "Sugira restaurantes populares entre locais em {cidade}"
+)
+
+modelo_cultural = ChatPromptTemplate.from_template(
+    "Sugira atividades e locais culturais em {cidade}"
+)
+
+parte1 = modelo_cidade | llm | parseador
+parte2 = modelo_restaurantes | llm | StrOutputParser()
+parte3 = modelo_cultural | llm | StrOutputParser()
+
+# cadeia = parte1 | parte2 | parte3
+cadeia = (parte1 | {
+    "restaurantes": parte2,
+    "cultural": parte3
+})
+
+resultado = cadeia.invoke({"interesse": "museus"})
+
+print(resultado)
---
+++ langchain_memoria.py
@@ -0,0 +1,41 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import SimpleSequentialChain, LLMChain, ConversationChain
+from langchain.globals import set_debug
+from langchain.memory import ConversationBufferMemory
+from operator import itemgetter
+import os
+from dotenv import load_dotenv
+from langchain_core.pydantic_v1 import Field, BaseModel
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+
+load_dotenv()
+set_debug(True)
+
+
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0.7,
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+mensagens = [
+        "Quero visitar um lugar no Brasil famoso por suas praias e cultura. Pode me recomendar?",
+        "Qual é o melhor período do ano para visitar em termos de clima?",
+        "Quais tipos de atividades ao ar livre estão disponíveis?",
+        "Alguma sugestão de acomodação eco-friendly por lá?",
+        "Cite outras 20 cidades com características semelhantes às que descrevemos até agora. Rankeie por mais interessante, incluindo no meio ai a que você já sugeriu.",
+        "Na primeira cidade que você sugeriu lá atrás, quero saber 5 restaurantes para visitar. Responda somente o nome da cidade e o nome dos restaurantes.",
+]
+
+memory = ConversationBufferMemory()
+conversation = ConversationChain(llm=llm,
+                                 memory=memory,
+                                 verbose=True)
+
+
+
+for mensagem in mensagens:
+    resposta = conversation.predict(input = mensagem)
+    print(resposta)
